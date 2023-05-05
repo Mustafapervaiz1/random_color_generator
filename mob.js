2cbdb022414357d87cf52368cbc61842ed215763
mustafapervez <syalani.training1@gmail.com>
2023-05-05 23:55:11 +0500

    final update

--- mob.js
+++ mob.js
@@ -1,20 +1,13 @@
-
-let getcolor = ()=>{ 
-
-const x  = Math.floor(Math.random()*16777215);
-const randomcode = "#" + x.toString(16) ;
-document.body.style.backgroundColor = randomcode;
-console.log(randomcode);
- document.getElementById('colorinwriten').innerText = randomcode ;
-  // automatic copy to clip board 
- navigator.clipboard.writeText(randomcode)
-}   
-             //Event call this is run on click event
-document.getElementById('btn').addEventListener('click',getcolor )
-           //initial call this is run on pageload
-getcolor()  
-
-                   
-
-
-
+let getcolor = () => {
+  const x = Math.floor(Math.random() * 16777215);
+  const randomcode = "#" + x.toString(16);
+  document.body.style.backgroundColor = randomcode;
+  console.log(randomcode);
+  document.getElementById("colorinwriten").innerText = randomcode;
+  // automatic copy to clip board
+  navigator.clipboard.writeText(randomcode);
+};
+//Event call this is run on click event
+document.getElementById("btn").addEventListener("click", getcolor);
+//initial call this is run on pageload
+getcolor();
